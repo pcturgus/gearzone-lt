0b0a75f63d2a6f60e4ea0ce7c7e2257c292a448b
Mantas <mantasraulinaitis@gmail.com>
2026-08-26 20:06:21 +0300

    Prideta Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -30,7 +31,10 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+            <body className={...}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
