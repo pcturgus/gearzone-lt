fafaed485bc461bddffb535fa5e70d4d26ffda03
Mantas <mantasraulinaitis@gmail.com>
2026-08-27 08:25:12 +0300

    Fix body className syntax error

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-            <body className={...}>
+      <body className="min-h-full flex flex-col">
         {children}
         <Analytics />
       </body>
